84e84990e881e5815dfe71c23b09ee3e01c7b8b1
Steve Greenberg <spgreenberg@gmail.com>
2014-04-25 11:35:30 -0600

    Added default main for spring boot happiness

---
+++ src/main/java/org/cloudfoundry/community/servicebroker/config/DefaultApplication.java
@@ -0,0 +1,17 @@
+package org.cloudfoundry.community.servicebroker.config;
+
+import org.springframework.boot.SpringApplication;
+
+
+/** 
+ * Default main for spring boot
+ * @author sgreenberg
+ *
+ */
+public class DefaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultApplication.class, args);
+    }
+    	
+}
