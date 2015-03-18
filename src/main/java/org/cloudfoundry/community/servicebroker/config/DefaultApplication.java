6d9b27d3703c509bb2cc3f0b737014d18bdb7280
krujos <jkruck@pivotal.io>
2015-03-18 12:40:18 -0500

    Undo jitpack changes. It seems to struggle with spring boot.

--- src/main/java/org/cloudfoundry/community/servicebroker/config/DefaultApplication.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/config/DefaultApplication.java
@@ -1,15 +1,12 @@
 package org.cloudfoundry.community.servicebroker.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 /** 
  * Default main for spring boot
  * @author sgreenberg
  *
  */
-@SpringBootApplication
 public class DefaultApplication {
 
     public static void main(String[] args) {
