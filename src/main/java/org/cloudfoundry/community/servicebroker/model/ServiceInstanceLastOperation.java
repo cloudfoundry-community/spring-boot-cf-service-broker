0633d7c641158387f47cb9b4273fa83d08009059
krujos <jkruck@pivotal.io>
2015-03-18 14:48:38 -0500

    empty constructor for construction last_operation object in tests

--- src/main/java/org/cloudfoundry/community/servicebroker/model/ServiceInstanceLastOperation.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/model/ServiceInstanceLastOperation.java
@@ -15,6 +15,8 @@ public class ServiceInstanceLastOperation {
 	
 	private OperationState state; 
 	
+	public ServiceInstanceLastOperation() {}
+	
 	public ServiceInstanceLastOperation(
 			final String description, 
 			final OperationState operationState)  { 
