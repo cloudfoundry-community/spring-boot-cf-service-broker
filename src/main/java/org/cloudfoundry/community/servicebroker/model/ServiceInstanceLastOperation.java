9bd32b4c3ac750288cfbaf9dc115064f35b08920
krujos <jkruck@pivotal.io>
2015-03-19 13:58:24 -0500

    Update docs

--- src/main/java/org/cloudfoundry/community/servicebroker/model/ServiceInstanceLastOperation.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/model/ServiceInstanceLastOperation.java
@@ -3,63 +3,81 @@ package org.cloudfoundry.community.servicebroker.model;
 import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+/**
+ * This class is used to indicate the status of the last action CloudController
+ * asked the broker to perform for a given service instance. The cloud
+ * controller understands three states: failed, in progress, and succeeded. the
+ * <code>description</code> is user facing.
+ * 
+ * @author jkruck
+ *
+ */
 @JsonAutoDetect
 public class ServiceInstanceLastOperation {
-	
+
 	@JsonSerialize
 	@JsonProperty("dashboard_url")
-	private String dashboarUrl; 
-	
+	private String dashboarUrl;
+
 	@JsonSerialize
 	private String description;
-	
-	private OperationState state; 
-	
-	public ServiceInstanceLastOperation() {}
-	
-	public ServiceInstanceLastOperation(
-			final String description, 
-			final OperationState operationState)  { 
-		setDescription(description); 
+
+	private OperationState state;
+
+	public ServiceInstanceLastOperation() {
+	}
+
+	/**
+	 * Create a new last operation state
+	 * 
+	 * @param description
+	 *            user facing text describing the operation
+	 * @param operationState
+	 *            reflecting the current phase of the operation life cycle.
+	 */
+	public ServiceInstanceLastOperation(final String description,
+			final OperationState operationState) {
+		setDescription(description);
 		this.state = operationState;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	private void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	@JsonSerialize
 	public String getState() {
-		switch (state) { 
-		case IN_PROGRESS: 
+		switch (state) {
+		case IN_PROGRESS:
 			return "in progress";
-		case SUCCEEDED: 
-			return "succeeded"; 
-		case FAILED: 
-			return "failed"; 
-		};
-		assert(false);
+		case SUCCEEDED:
+			return "succeeded";
+		case FAILED:
+			return "failed";
+		}
+		;
+		assert (false);
 		return "internal error";
 	}
-	
+
 	@JsonSerialize
-	private void setState(String state) { 
-		switch(state) { 
-		case "in progress": 
+	private void setState(String state) {
+		switch (state) {
+		case "in progress":
 			this.state = OperationState.IN_PROGRESS;
 			break;
-		case "succeeded": 
-			this.state = OperationState.SUCCEEDED; 
-			break; 
+		case "succeeded":
+			this.state = OperationState.SUCCEEDED;
+			break;
 		case "failed":
 			this.state = OperationState.FAILED;
-			break; 
+			break;
 		default:
-			assert(false);
+			assert (false);
 			break;
 		}
 	}
--- src/main/java/org/cloudfoundry/community/servicebroker/service/ServiceInstanceService.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/service/ServiceInstanceService.java
@@ -4,50 +4,95 @@ import org.cloudfoundry.community.servicebroker.exception.*;
 import org.cloudfoundry.community.servicebroker.model.*;
 
 /**
- * Handles instances of service definitions.
+ * Handles instances of service definitions. If your broker requires async
+ * provisioning, deprovisioning or updates you must call
+ * <code>isAsync(true)</code> on the returned <code>ServiceInstance</code>
+ * objects from those methods.
  * 
  * @author sgreenberg@gopivotal.com
  */
 public interface ServiceInstanceService {
 
 	/**
-	 * Create a new instance of a service
-	 * @param createServiceInstanceRequest containing the parameters from CloudController
-	 * @return The newly created ServiceInstance
-	 * @throws ServiceInstanceExistsException if the service instance already exists.
-	 * @throws ServiceBrokerException if something goes wrong internally
-	 * @throws ServiceBrokerAsyncRequiredException if we must use an async comparable cloud controller
+	 * Create a new instance of a service.
+	 * 
+	 * @param createServiceInstanceRequest
+	 *            containing the parameters from CloudController, if the cloud
+	 *            controller is capable of async provisioning
+	 *            <code>request.hasAsyncClient()</code> will return true.
+	 * @return The newly created ServiceInstance. If your broker requires an
+	 *         asynchronous provisioning call <code>isAsync(true)</code> on the service
+	 *         instance, otherwise synchronous provisioning is assumed.
+	 * @throws ServiceInstanceExistsException
+	 *             if the service instance already exists.
+	 * @throws ServiceBrokerException
+	 *             if something goes wrong internally
+	 * @throws ServiceBrokerAsyncRequiredException
+	 *             if async provisioning is required but the incoming
+	 *             <code>request</code> object did not indicate an async
+	 *             CloudController
+	 * 
+	 * @see org.cloudfoundry.community.servicebroker.model.ServiceInstance
+	 * @see org.cloudfoundry.community.servicebroker.model.CreateServiceInstanceRequest
 	 */
 	ServiceInstance createServiceInstance(CreateServiceInstanceRequest createServiceInstanceRequest) 
 			throws ServiceInstanceExistsException, ServiceBrokerException, ServiceBrokerAsyncRequiredException;
 	
 	/**
-	 * @param serviceInstanceId The id of the serviceInstance
-	 * @return The ServiceInstance with the given id or null if one does not exist
+	 * @param serviceInstanceId
+	 *            The id of the serviceInstance
+	 * @return The ServiceInstance with the given id or null if one does not
+	 *         exist. During async provisioning and deprovision it is expected
+	 *         that the returned <code>ServiceInstance</code> will have an up to
+	 *         date <code>ServiceInstanceLastOperation</code>. This is the
+	 *         method that the CloudController uses to determine if async
+	 *         provisioning is complete. During an async deprovision, it is
+	 *         expected this method will return null after a successful
+	 *         deprovision.
+	 * 
+	 * @see org.cloudfoundry.community.servicebroker.model.ServiceInstance
+	 * @see org.cloudfoundry.community.servicebroker.model.ServiceInstanceLastOperation
 	 */
 	ServiceInstance getServiceInstance(String serviceInstanceId);
 	
 	/**
 	 * Delete and return the instance if it exists.
-	 * @param deleteServiceInstanceRequest containing pertinent information for deleting the service.
-	 * @return The deleted ServiceInstance or null if one did not exist.
-	 * @throws ServiceBrokerException is something goes wrong internally
-	 * @throws ServiceBrokerAsyncRequiredException if we must use an async comparable cloud controller
+	 * 
+	 * @param deleteServiceInstanceRequest
+	 *            containing pertinent information for deleting the service.
+	 * @return The deleted ServiceInstance or null if one did not exist. If your
+	 *         broker requires an asynchronous deprovisioning call
+	 *         <code>isAsync(true)</code> on the service instance, otherwise
+	 *         synchronous provisioning is assumed.
+	 * @throws ServiceBrokerException
+	 *             is something goes wrong internally
+	 * @throws ServiceBrokerAsyncRequiredException
+	 *             if we must use an async comparable cloud controller
 	 * 
 	 */
 	ServiceInstance deleteServiceInstance(DeleteServiceInstanceRequest deleteServiceInstanceRequest) 
 			throws ServiceBrokerException, ServiceBrokerAsyncRequiredException;
 
 	/**
-	 * Update a service instance. Only modification of service plan is supported.
-	 * @param updateServiceInstanceRequest detailing the request parameters
+	 * Update a service instance. Only modification of service plan is
+	 * supported.
 	 * 
-	 * @return The updated serviceInstance
-	 * @throws ServiceInstanceUpdateNotSupportedException if particular plan change is not supported
-	 *         or if the request can not currently be fulfilled due to the state of the instance.
-	 * @throws ServiceInstanceDoesNotExistException if the service instance does not exist
-	 * @throws ServiceBrokerException if something goes wrong internally
-	 * @throws ServiceBrokerAsyncRequiredException if we must use an async comparable cloud controller
+	 * @param updateServiceInstanceRequest
+	 *            detailing the request parameters
+	 * 
+	 * @return The updated serviceInstance. If your broker requires an
+	 *         asynchronous updates call <code>isAsync(true)</code> on the
+	 *         service instance, otherwise synchronous provisioning is assumed.
+	 * @throws ServiceInstanceUpdateNotSupportedException
+	 *             if particular plan change is not supported or if the request
+	 *             can not currently be fulfilled due to the state of the
+	 *             instance.
+	 * @throws ServiceInstanceDoesNotExistException
+	 *             if the service instance does not exist
+	 * @throws ServiceBrokerException
+	 *             if something goes wrong internally
+	 * @throws ServiceBrokerAsyncRequiredException
+	 *             if we must use an async comparable cloud controller
 	 * 
 	 */
 	ServiceInstance updateServiceInstance(UpdateServiceInstanceRequest updateServiceInstanceRequest)
